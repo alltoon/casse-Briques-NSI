3be6f0a981874936d4f5301ea7f484a34ba44a2a
skyfllex <84990508+alltoon@users.noreply.github.com>
2021-10-20 15:57:29 +0200

    Update balle.py
    rajout de comentaires

--- balle.py
+++ balle.py
@@ -4,16 +4,16 @@ from random import randint
 noir = (0, 0, 0)  # définition de la couleur noir pour la même raison
 
 
-class Balle(pygame.sprite.Sprite):  # notre classe raquette
+class Balle(pygame.sprite.Sprite):  # notre classe balle
     def __init__(self, couleur, rayon):
         super().__init__()
 
         self.image = pygame.Surface([rayon * 2, rayon * 2])
-        self.image.fill(noir)
+        self.image.fill(noir)   # image de fond en noir
         self.image.set_colorkey(noir)
 
-        pygame.draw.circle(self.image, couleur, [rayon, rayon], rayon)
-        self.velocity = [randint(4, 5), randint(-8, 8)]
+        pygame.draw.circle(self.image, couleur, [rayon, rayon], rayon)      # On dessine le cercle
+        self.velocity = [randint(4, 5), randint(-8, 8)]     # on set la direction de la balle aléatoirement
 
         self.rect = self.image.get_rect()
 
